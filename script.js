82201b8b8bf9c1b9fef47913c061f1a628e074c7
onodera <onodera@openmailbox.org>
2016-02-01 19:11:52 +0100

    new

--- script.js
+++ script.js
@@ -1,58 +1,29 @@
 $(document).ready(function(){
-	$('h1').waypoint(function(direction) {
+	$('#border:nth-child(2)').waypoint(function(direction) {
 		if (direction === 'down') {
-			$('#border:first-child img').animate({
-				opacity: "0"				
-			}, 200 );
-		} else {
-			$('#border:first-child img').animate({
-				opacity: "1"
-			}, 200 );
-		}
-	}, { offset: -400 });
-
-	$('hr').waypoint(function(direction) {
-		if (direction === 'down') {
-			setTimeout(function(){
-				$('#border').animate({
-					opacity: "0"
-				}, 50 );
-			}, 50);
-
-			$('#border').animate({
-				backgroundColor: "#FFFFFF",
-				width: "300px"
-			}, 150 );
-			$('#border h2').animate({
-				opacity: "0"
-			}, 50 );
 			$('#header').animate({
+				backgroundColor: "#F8C6AD",
+				height: "70px",
+				width: "70px"
+			}, 300 );
+			$('h1, h2').animate({
+				opacity: "0"
+			}, 200 );
+			$('h1, h2').css({
+				"display" : "none"
+			});
+		} else {
+			$('#header').animate({
+				backgroundColor: "#F9F7F6",
+				height: "150px",
+				width: "325px"
+			}, 300 );
+			$('h1, h2').animate({
 				opacity: "1"
 			}, 200 );
-		} else {
-			setTimeout(function(){
-				$('#border').animate({
-					backgroundColor: "#F6F6F6",
-					width: $("body").width() - 114 + "px"
-				}, 150 );
-				$('#border h2').animate({
-					opacity: "1"
-				}, 400 );
-				$('#header').animate({
-					opacity: "0"
-				}, 50 );
-			}, 50);
-
-			$('#border').animate({
-				opacity: "1"
-			}, 50 );
+			$('h1, h2').css({
+				"display" : "block"
+			});
 		}
-	}, { offset: 180 });
-});
-
-
-$(window).resize(function() {
-		$('#border').css({
-			"width": $("body").width() - 114 + "px"
-		}, 150 );
+	}, { offset: 300 });
 });
